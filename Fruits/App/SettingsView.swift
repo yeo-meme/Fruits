a11f1813b882129ed6c7d30ecb8552ccaa2c2869
yeomeme <yeomeme@gmail.com>
2022-11-10 17:53:21 +0900

    ux -> 토글 전환시 (restart, restarted) 텍스트변경

--- Fruits/App/SettingsView.swift
+++ Fruits/App/SettingsView.swift
@@ -49,9 +49,20 @@ struct SettingsView: View {
                             .multilineTextAlignment(.leading)
                         
                         Toggle(isOn: $isOnboarding) {
-                            Text("Restart".uppercased())
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.secondary)
+                            }
                         }
-                        
+                        .padding()
+                        .background(Color(UIColor.tertiarySystemBackground)
+                        .clipShape(RoundedRectangle(cornerRadius: 8,style: .continuous))
+                        )
                     }
                         
                         
